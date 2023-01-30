f2aed58c0086f5168efcfc1fea015d757504e9ff
José Luiz Fagundes <69995096+jlfagundes@users.noreply.github.com>
2023-01-30 23:21:50 +0000

    feat: ts form status

--- angular-multistepform-angularservices/src/app/components/shared/form-status/form-status.component.ts
+++ angular-multistepform-angularservices/src/app/components/shared/form-status/form-status.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { UserFormDomainService } from 'src/app/services/user-form-domain.service';
 
 @Component({
   selector: 'app-form-status',
   templateUrl: './form-status.component.html',
   styleUrls: ['./form-status.component.css']
 })
-export class FormStatusComponent {
+export class FormStatusComponent implements OnInit {
+  
+  @Input()
+  currentStep!: number;
 
+  get totalSteps(): number {
+    return this.userFormDomainService.totalSteps;
+  }
+
+  get steps(): number[] {
+    return Array(this.totalSteps)
+      .fill(0)
+      .map((value, index) => index + 1);
+  }
+
+  constructor(
+    private userFormDomainService: UserFormDomainService
+  ) { }
+  ngOnInit(): void {}
 }
