6d1d4853660a786aabbe20578cc52a153121b895
José Luiz Fagundes <69995096+jlfagundes@users.noreply.github.com>
2023-01-30 23:21:28 +0000

    feat: service

--- angular-multistepform-angularservices/src/app/services/user-form-domain.service.ts
+++ angular-multistepform-angularservices/src/app/services/user-form-domain.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { UserFormModel } from '../model/domain/user-form-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserFormDomainService {
 
-  constructor() { }
+  model: UserFormModel;
+  readonly totalSteps: number;
+
+  constructor() {
+    this.totalSteps = 3;
+    this.model = {
+      userModel: {} as any,
+      addressModel: {} as any,
+      topicsModel: {} as any,
+   };
+  }
 }
