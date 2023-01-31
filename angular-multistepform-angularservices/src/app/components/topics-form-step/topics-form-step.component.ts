4d6d4467545dea5b9b1d5fbd99e821bcba16598d
José Luiz Fagundes <69995096+jlfagundes@users.noreply.github.com>
2023-01-31 00:25:36 +0000

    feat: topics component

--- angular-multistepform-angularservices/src/app/components/topics-form-step/topics-form-step.component.ts
+++ angular-multistepform-angularservices/src/app/components/topics-form-step/topics-form-step.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserFormModel } from 'src/app/model/domain/user-form-model';
+import { UserFormDomainService } from 'src/app/services/user-form-domain.service';
 
 @Component({
   selector: 'app-topics-form-step',
@@ -6,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./topics-form-step.component.css']
 })
 export class TopicsFormStepComponent {
+  get userFormModel(): UserFormModel {
+    return this.userFormDomainService.model;
+  }
 
+  get availableTopics(): string[] {
+    return [
+      'Java',
+      'C#',
+      'TypeScript',
+      'C++',
+      'C',
+      'Python',
+      'Ruby',
+      'SQL'
+    ];
+  }
+
+  constructor(
+    private userFormDomainService: UserFormDomainService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+  ngOnInit(): void {}
+
+  goNext(): void {
+    this.router.navigate(['..', 'result'], { relativeTo: this.route});
+  }
 }
