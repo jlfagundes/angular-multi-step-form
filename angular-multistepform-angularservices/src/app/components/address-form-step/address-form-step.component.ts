7c917b8d891b643e5db5c6f1725eaa1cf81530f8
José Luiz Fagundes <69995096+jlfagundes@users.noreply.github.com>
2023-01-31 00:24:37 +0000

    feat: adress component

--- angular-multistepform-angularservices/src/app/components/address-form-step/address-form-step.component.ts
+++ angular-multistepform-angularservices/src/app/components/address-form-step/address-form-step.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserFormModel } from 'src/app/model/domain/user-form-model';
+import { UserFormDomainService } from 'src/app/services/user-form-domain.service';
 
 @Component({
   selector: 'app-address-form-step',
@@ -6,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./address-form-step.component.css']
 })
 export class AddressFormStepComponent {
+  get userFormModel(): UserFormModel {
+    return this.userFormDomainService.model;
+  }
 
+  constructor(
+    private userFormDomainService: UserFormDomainService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+  ngOnInit(): void {}
+
+  goNext(): void {
+    this.router.navigate(['..', 'topics'], { relativeTo: this.route});
+  }
 }
