bbf6eccc43e46f85697358bb0c27ae2e63425a02
José Luiz Fagundes <69995096+jlfagundes@users.noreply.github.com>
2023-01-31 00:25:49 +0000

    feat: user component

--- angular-multistepform-angularservices/src/app/components/user-form-step/user-form-step.component.ts
+++ angular-multistepform-angularservices/src/app/components/user-form-step/user-form-step.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserFormModel } from 'src/app/model/domain/user-form-model';
+import { UserFormDomainService } from 'src/app/services/user-form-domain.service';
 
 @Component({
   selector: 'app-user-form-step',
   templateUrl: './user-form-step.component.html',
   styleUrls: ['./user-form-step.component.css']
 })
-export class UserFormStepComponent {
+export class UserFormStepComponent implements OnInit{
+  get userFormModel(): UserFormModel {
+    return this.userFormDomainService.model;
+  }
 
+  constructor(
+    private userFormDomainService: UserFormDomainService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+  ngOnInit(): void {}
+
+  goNext(): void {
+    this.router.navigate(['..', 'address'], { relativeTo: this.route});
+  }
 }
